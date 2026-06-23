bc6648c26216e984b8db44f74df736be25b8fbeb
KIM-YOUNG-CHAE <kyco199610@gmail.com>
2026-06-23 21:50:11 +0900

    JS 4일차

---
+++ day04/switch02.js
@@ -0,0 +1,25 @@
+let mbti = ""
+
+switch(mbti){
+    case "ISTJ":
+        console.log("재미 없음")
+        break;
+    case "INTP":
+        console.log("공감능력 없음")
+        break;
+    case "INFP":
+        console.log("배려 없음")
+        break;
+    case "INTJ":
+        console.log("오만함")
+        break;
+    case "INFJ":
+        console.log("위험함")
+        break;
+    case "ISTP":
+        console.log("배려심 부족")
+        break;
+    default:
+        console.log("신남")
+        break;
+}
